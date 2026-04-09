2df7d6a52d225004e5649b4384ff53628f419654
sai kumar <iamsaikumar24@gmail.com>
2026-04-09 05:51:18 +0000

    added variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,29 @@
 variable "region" {
-  default = "ap-south-1"
+  description = "AWS region to deploy into"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Name for the EKS cluster and associated resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of AZs to use (must have at least 2)"
+  type        = list(string)
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for EKS worker nodes"
+  type        = string
+}
+
+variable "node_count" {
+  description = "Number of EKS worker nodes"
+  type        = number
 }
